3cabdc83b4bee27e8d18022e22693b94388173ff
EGOR <barbaris9010@gmail.com>
2020-11-11 22:38:18 +0300

    new one

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
+let date = new Date();
 let title = document.head.children[1];
 let name = document.querySelector('h1.user__name');
 let bio = document.querySelector('p.user-info__description');
 let div = document.querySelector('div.user-info');
+let time = document.querySelector('.time');
+let searchParams = new URLSearchParams(window.location.search);
+let login = searchParams.get('username');
+let preload = document.querySelector('.preloader')
+let apiUrl = 'https://api.github.com/users/' + login;
 let pic;
 let link;
 
-let searchParams = new URLSearchParams(window.location.search);
-let login = searchParams.get('username');
 
-let apiUrl = 'https://api.github.com/users/' + login;
-fetch(apiUrl)
+const getDate = new Promise((resolve, reject) => {
+    setTimeout(() => date ? resolve(date) : reject("Out of date"), 1500);
+});
+const getName = new Promise((resolve, reject) => {
+      setTimeout(() => login ? resolve(login) : reject('имя не найдено, отображается пользователь по умолчанию'), 3000);
+
+const preloader = setTimeout(() => {
+      document.body.classList.add('loaded_hiding');
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+      }, 3000);
+
+function UserInfo(Name){
+    let request = fetch(apiUrl)
     .then(response => response.json())
     .then(user => { 
         if (user.name) {
@@ -20,17 +36,16 @@ fetch(apiUrl)
             bio.innerHTML = 'No bio.' + '/n':
             bio.innerHTML = user.bio +'. ';
 
-            link = document.createElement('a');
-            link.innerHTML = 'Показать профиль';
-            link.setAttribute('href', user.html_url);
-            bio.appendChild(link);
-
             pic = document.createElement('img');
             pic.className = 'user-info__pic';
             pic.setAttribute('alt', 'User Avatar');
             pic.setAttribute('src', user.avatar_url);
             div.insertBefore(pic, div.children[0]);
 
+            time.innerHTML = date;
         }
     })
-    .catch(error => console.log(error)); 
+    Promise.all([getDate, getName, request])}
+    if (searchParams.has('username')) {
+              UserInfo(login);
+};
